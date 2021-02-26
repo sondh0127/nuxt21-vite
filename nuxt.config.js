cb0fdfca7431102365b54674ea83862b0a7a556c
Son Hong Do <sondh0127@gmail.com>
2021-02-26 21:53:24 +0700

    add nuxt vite

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
+    'nuxt-vite'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -38,5 +39,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+  telemetry: false
 }
